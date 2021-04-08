14b2b74dfd643536dad1950a606b17e91da5c5bf
Julian Harrington <jrayharrington@gmail.com>
2021-04-08 17:01:25 +1000

    add property arrow function to stop form from submitting, get text input from value, change page to correct path with push state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,31 @@
 import React, { Fragment } from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get text from the input on the form
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-    <Fragment>
-      <form className="store-selector">
-        { /* This is a comment in JSX */ }
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
-    </Fragment>
     )
   }
 }
