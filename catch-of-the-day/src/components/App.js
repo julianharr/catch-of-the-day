44070a0676403fe13aa97a9a6d6bdc101816210b
Julian Harrington <jrayharrington@gmail.com>
2021-04-10 13:39:10 +1000

    css transitions for orders, delete orders

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -61,6 +61,15 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  deleteFish = (key) => {
+    // 1. Take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. Update the state
+    fishes[key] = null;
+    // 3. Update state
+    this.setState({ fishes });
+  }
+
   loadSampleFishes = () => {
     // What is the piece of state to update? fishes. What do we
     // want to update it with? sampleFishes.
@@ -73,6 +82,15 @@ class App extends React.Component {
     const order = { ...this.state.order };
     // 2. Either add to the order or update the number in our order
     order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  }
+
+  removeFromOrder = (key) => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either remove from the order or update the number in our order
+    delete order[key];
     // 3. Call setState to update our state object
     this.setState({ order });
   }
@@ -97,11 +115,13 @@ class App extends React.Component {
         <Order
           fishes={this.state.fishes}
           order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
         />
 
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
